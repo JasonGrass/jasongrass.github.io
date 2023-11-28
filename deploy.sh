d0ddf28de06168b84ee108da47647527edd8c384
JasonGrass <junjiegrass@qq.com>
2023-11-28 10:44:01 +0800

    添加发布脚本

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# [About "error:0308010C:digital envelope routines::unsupported" · Issue #3095 · vuejs/vuepress](https://github.com/vuejs/vuepress/issues/3095 )
+# set NODE_OPTIONS=--openssl-legacy-provider
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd ./dist
+
+# 如果是发布到自定义域名
+# echo 'blog.jgrass.xyz' > CNAME
+
+time=$(date "+%Y%m%d-%H%M%S")
+message="deploy$time"
+
+git init
+git config user.email "junjiegrass@qq.com"
+git config user.name "Jasongrass"
+git add -A
+git commit -m $message
+git checkout -b public
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:JasonGrass/jasongrass.github.io.git public
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 发布到 gitee 使用 github actions 完成
+# git push -f git@gitee.com:Jasongrass/Jasongrass.git master
+
+cd -
